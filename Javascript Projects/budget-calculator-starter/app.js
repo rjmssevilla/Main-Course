eba3b9301fb118341882b2f1437c3adea5e3c2b5
Rio Sevilla <rjmssevilla@gmail.com>
2020-12-18 15:59:16 +0400

    added current month display

--- Javascript Projects/budget-calculator-starter/app.js
+++ Javascript Projects/budget-calculator-starter/app.js
@@ -1,1 +1,26 @@
+//functions
+function getCurrentMonth(currentMonth){
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+
+  var d = new Date();
+  var n = month[d.getMonth()];
+  currentMonth.innerHTML = n;
+}
+
 //variable declarations
+var displayMonth = document.querySelector(".budget__title--month");
+
+
+getCurrentMonth(displayMonth);
