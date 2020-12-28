ba417ea4bcb880ea3a07005776611caec94e52ad
Rio Sevilla <rjmssevilla@gmail.com>
2020-12-28 23:43:49 +0400

    added array insertions

--- Javascript Projects/budget-calculator-starter/app.js
+++ Javascript Projects/budget-calculator-starter/app.js
@@ -1,3 +1,4 @@
+(function(){
 //functions
 function getCurrentMonth(currentMonth){
   var month = new Array();
@@ -15,12 +16,70 @@ function getCurrentMonth(currentMonth){
   month[11] = "December";
 
   var d = new Date();
-  var n = month[d.getMonth()];
-  currentMonth.innerHTML = n;
+  currentMonth.innerHTML = month[d.getMonth()];
 }
+
+var Income = function(id, description, value) {
+  this.id = id;
+  this.description = description;
+  this.value = value;
+};
+
+var Expense = function(id, description, value) {
+  this.id = id;
+  this.description = description;
+  this.value = value;
+  this.percentage = -1;
+};
+
+function insertIntoIncomeArray (item){
+  incArray.push(item);
+};
+
+function insertIntoExpenseArray (item){
+  expArray.push(item);
+};
+
+
 
 //variable declarations
 var displayMonth = document.querySelector(".budget__title--month");
+var totalBudget = document.querySelector(".budget__value");
+var addButton = document. querySelector(".add__btn");
+var addType = document.querySelector(".add__type");
+var addDescription = document.querySelector(".add__description");
+var addValue = document.querySelector(".add__value");
+
+getCurrentMonth(displayMonth); //get and display current month
+
+var incID = 0;
+var expID = 0;
+var newItem;
+var incArray = [];
+var expArray = [];
+
+addButton.addEventListener('click', e => {
+  if (addType.value == "inc"){
+    newItem = new Income (incID, addDescription.value,addValue.value)
+    incID = incID+1;
+    insertIntoIncomeArray(newItem)
+  }
+  else if (addType.value =="exp"){
+    newItem = new Expense (expID, addDescription.value,addValue.value)
+    expID = expID+1;
+    insertIntoExpenseArray(newItem)
+  }
+
+  for (var i = 0; i < incArray.length; i++) {
+    console.log(incArray[i])
+  }
+
+  for (var i = 0; i < expArray.length; i++) {
+    console.log(expArray[i])
+  }
+
+})
+})();
 
 
-getCurrentMonth(displayMonth);
+
