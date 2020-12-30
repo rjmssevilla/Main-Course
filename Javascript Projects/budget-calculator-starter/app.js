54de5d1c0eea960e5f6e4558c938d056ddc29661
Rio Sevilla <rjmssevilla@gmail.com>
2020-12-31 01:22:44 +0400

    added dynamic div function

--- Javascript Projects/budget-calculator-starter/app.js
+++ Javascript Projects/budget-calculator-starter/app.js
@@ -60,11 +60,90 @@ function updateBudget(income, expense){
   totalBudget.textContent = budget;
 }
 
-function createIncomeElements(){
-
+function resetFields(){
+  addDescription.value = "";
+  addValue.value = "";
 }
 
-//variable declarations
+function createIncomeElements(){
+  var incomeDiv = document.createElement("div");
+  incomeDiv.classList.add("item", "clearfix");
+  incomeDiv.id = "income-" +incID;
+
+  var itemDescriptionDiv = document.createElement("div");
+  itemDescriptionDiv.classList.add("item__description")
+  
+  var rightClearfix = document.createElement("div");
+  rightClearfix.classList.add("right", "clearfix");
+
+  var itemValue = document.createElement("div");
+  itemValue.classList.add("item__value");
+
+  var itemDelete = document.createElement("div");
+  itemDelete.classList.add("item__delete");
+
+  var itemDeleteButton = document.createElement("button");
+  itemDeleteButton.classList.add("item__delete--btn");
+
+  var ionOutline = document.createElement("i");
+  ionOutline.classList.add("ion-ios-close-outline");
+  // Append the children to div
+  incomeList.appendChild(incomeDiv); //append item clearfix to income__list
+  incomeDiv.appendChild(itemDescriptionDiv); //append item__description to item clearfix
+  incomeDiv.appendChild(rightClearfix); //append right clearfix to item clearfix
+  rightClearfix.appendChild(itemValue); //append item__value to right clearfix
+  rightClearfix.appendChild(itemDelete); //append item__delete to right Clearfix
+  itemDelete.appendChild(itemDeleteButton); //append item delete button to item__delete
+  itemDeleteButton.appendChild(ionOutline); //append ion outline to item delete button
+
+  //place inputs
+  var itemDescription = document.createTextNode(addDescription.value);
+  itemDescriptionDiv.appendChild(itemDescription);
+
+  var itemValueInput = document.createTextNode("+ " + addValue.value);
+  itemValue.appendChild(itemValueInput);
+}
+
+function createExpenseElements(){
+  var incomeDiv = document.createElement("div");
+  incomeDiv.classList.add("item", "clearfix");
+  incomeDiv.id = "expense-" +expID;
+
+  var itemDescriptionDiv = document.createElement("div");
+  itemDescriptionDiv.classList.add("item__description")
+  
+  var rightClearfix = document.createElement("div");
+  rightClearfix.classList.add("right", "clearfix");
+
+  var itemValue = document.createElement("div");
+  itemValue.classList.add("item__value");
+
+  var itemDelete = document.createElement("div");
+  itemDelete.classList.add("item__delete");
+
+  var itemDeleteButton = document.createElement("button");
+  itemDeleteButton.classList.add("item__delete--btn");
+
+  var ionOutline = document.createElement("i");
+  ionOutline.classList.add("ion-ios-close-outline");
+  // Append the children to div
+  expenseList.appendChild(incomeDiv); //append item clearfix to income__list
+  incomeDiv.appendChild(itemDescriptionDiv); //append item__description to item clearfix
+  incomeDiv.appendChild(rightClearfix); //append right clearfix to item clearfix
+  rightClearfix.appendChild(itemValue); //append item__value to right clearfix
+  rightClearfix.appendChild(itemDelete); //append item__delete to right Clearfix
+  itemDelete.appendChild(itemDeleteButton); //append item delete button to item__delete
+  itemDeleteButton.appendChild(ionOutline); //append ion outline to item delete button
+
+  //place inputs
+  var itemDescription = document.createTextNode(addDescription.value);
+  itemDescriptionDiv.appendChild(itemDescription);
+
+  var itemValueInput = document.createTextNode("+ " + addValue.value);
+  itemValue.appendChild(itemValueInput);
+}
+
+//variable declarations of html elements
 var displayMonth = document.querySelector(".budget__title--month");
 var totalBudget = document.querySelector(".budget__value");
 var addButton = document. querySelector(".add__btn");
@@ -73,9 +152,11 @@ var addDescription = document.querySelector(".add__description");
 var addValue = document.querySelector(".add__value");
 var budgetIncome = document.querySelector(".budget__income--value");
 var expenseIncome = document.querySelector(".budget__expenses--value");
+var incomeList = document.querySelector(".income__list")
+var expenseList = document.querySelector(".expenses__list")
+var closeButton = document.getElementsByClassName("item__delete--btn");
 
-getCurrentMonth(displayMonth); //get and display current month
-
+//variable declarations for semantics
 var incID = 0;
 var expID = 0;
 var incomeSum = 0;
@@ -84,6 +165,7 @@ var newItem;
 var incArray = [];
 var expArray = [];
 
+getCurrentMonth(displayMonth); //get and display current month
 
 addButton.addEventListener('click', e => {
 
@@ -92,8 +174,7 @@ addButton.addEventListener('click', e => {
     incID = incID+1;
     insertIntoIncomeArray(newItem);
     budgetIncome.textContent = addIncomes(incomeSum);
-
-    
+    createIncomeElements();
   }
 
   else if (addType.value =="exp"){
@@ -101,9 +182,24 @@ addButton.addEventListener('click', e => {
     expID = expID+1;
     insertIntoExpenseArray(newItem)
     expenseIncome.textContent = addExpenses(expenseSum);
+    createExpenseElements();
   }
 
+    
+  
+
+  var i;
+  for (i = 0; i < closeButton.length; i++) {
+    closeButton[i].onclick = function() {
+    var div = document.getElementById(this.parentNode.parentNode.parentNode.id);
+    alert(this.parentNode.parentNode.parentNode.id)
+    div.outerHTML="";
+    updateBudget(parseInt(budgetIncome.textContent), parseInt(expenseIncome.textContent));
+  }
+}
+
   updateBudget(parseInt(budgetIncome.textContent), parseInt(expenseIncome.textContent));
+  resetFields();
 })
 })();
 
