3e83172dd43d49ad9cc7d819ed5f4e45b193a3cd
Rio Sevilla <rjmssevilla@gmail.com>
2021-01-22 17:12:53 +0400

    updated activity 4

--- Ruby_Projects/Activities/Activity_2/number_4.rb
+++ Ruby_Projects/Activities/Activity_2/number_4.rb
@@ -1,10 +1,10 @@
 arr = [6,3,1,8,4,2,10,65,102]
 new_Arr = []
 
-arr.each { |item|
-    if (item % 2 == 0)
-      new_Arr << item
-    end
-}
+arr.each do |item|
+  if (item % 2 == 0)
+    new_Arr << item
+  end
+end
 
 puts new_Arr
