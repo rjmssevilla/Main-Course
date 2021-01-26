3b8b4f41b187d6aa783cbdcbe30ec225fda000c8
Rio Sevilla <rjmssevilla@gmail.com>
2021-01-26 16:14:56 +0400

    added activity 1 for sql

---
+++ Ruby_Projects/SQL_Activities/activity_1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE students (id integer, first_name VARCHAR(20), middle_name VARCHAR(20), last_name VARCHAR(20), age integer, location VARCHAR(50));
+
+INSERT INTO students (id, first_name, middle_name,last_name,age,location) 
+	VALUES
+    (1,"Juan","","Cruz",18,"Manila"),
+    (2,"John","","Young",20,"Manila"),
+    (3,"Victor","","Rivera",21,"Manila"),
+    (4,"Adrian","","Co",19,"Laguna"),
+    (5,"Pau","","Riosa",22,"Marikina"),
+    (6,"Piolo","","Pascual",25,"Manila");
+    
+ UPDATE students 
+	SET first_name = "Ana", middle_name = "Cui", last_name = "Cajocson", age = 25, location = "Bulacan"
+	WHERE id = 1
+    
+ DELETE FROM students WHERE id = 6
